8d1f4e6394a5fea41ea9bbb5cc2be2788b2f6f96
wizdomic <naskarrahul620@gmail.com>
2025-11-16 20:48:27 +0530

    k

--- expense-tracker/src/components/TransactionList/TransactionList.jsx
+++ expense-tracker/src/components/TransactionList/TransactionList.jsx
@@ -7,7 +7,7 @@ export default function TransactionList() {
 
   return (
     <div className={styles.listSection}>
-      <h3>Transaction History</h3>
+      <h3 className={styles.listSection}>Transaction History</h3>
 
       <ul className={styles.list}>
         {transactions.map((t) => (
